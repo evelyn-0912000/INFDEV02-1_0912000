0b27048aa3846c53f2beda395c3d97e8ec644884
evelyn-0912000 <evelyn.vdkmp@gmail.com>
2015-09-15 15:24:45 +0200

    Update AssignmentsDev.py

--- AssignmentsDev/AssignmentsDev.py
+++ AssignmentsDev/AssignmentsDev.py
@@ -1,2 +1,22 @@
-name = raw_input("What is your name?")
-print "Hi " + name + "."
+name = raw_input("What is your name?\n> ")
+print "Hi " + name + '.'
+
+try:
+    length = int(raw_input("How long do you wish your square to be (in asterisks)?\n> "))
+
+    if length > 4:
+        for line in range(0, length):
+            content = ""
+            column = 0
+            for column in range(0, length):
+                if length - line - column > 1 or length - column == 1 or length - line == 1:
+                    content = content + '*'
+                else:
+                    content = content + ' '
+            print content
+
+    else:
+        print str(length) + " is not a valid number. Try an integer of five or larger."
+
+except ValueError:
+    print("That's not a valid number, wrong type. Try again.")
